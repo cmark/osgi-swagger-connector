710c0576c3b7900f61349b792e82af5c355254c1
CMark <mczotter@b2international.com>
2014-10-10 16:32:28 +0200

    [build] added basic JUnit test case to osgi.example project.
    Wired in JUnit dependency.

---
+++ examples/com.cmark.swagger.osgi.example/src/com/cmark/swagger/osgi/example/HelloResourceTest.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2014 B2i Healthcare. All rights reserved.
+ *******************************************************************************/
+package com.cmark.swagger.osgi.example;
+
+import org.junit.Test;
+
+/**
+ * @author mczotter
+ * @since 1.0
+ */
+public class HelloResourceTest {
+
+	@Test
+	public void whenGettingTheApiDocsExpectHelloResourceThere() throws Exception {
+		
+	}
+	
+}
